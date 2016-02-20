4d859ab88e91d16b12ee3229673c048a3eae7cbb
Languard Gemstrike <languard@gmail.com>
2016-02-20 06:42:01 -0600

    Initial Setup

---
+++ Assets/Code/UI_Credits.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Credits : MonoBehaviour
+{
+
+    public void ClickReturnButton()
+    {
+        int targetLevel = PlayerPrefs.GetInt("TargetLevel");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(targetLevel);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PCPotions
+
+Pepper and Carrot created by David Revoy
+Artwork is licensed through CC-BY
+Sources can be found at www.pepperandcarrot.com
